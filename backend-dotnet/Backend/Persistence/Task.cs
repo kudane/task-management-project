61d1859309e98ffe509d3c95e00fa44310e4a080
Kraisorn.R <kraisorn204@gmail.com>
2023-12-25 14:13:47 +0700

    [update] change fk name

--- backend-dotnet/Backend/Persistence/Task.cs
+++ backend-dotnet/Backend/Persistence/Task.cs
@@ -1,25 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Backend.Persistence;
+﻿namespace Backend.Persistence;
 
 public partial class Task
 {
     public int Id { get; set; }
-
     public string? Name { get; set; }
-
     public string? Description { get; set; }
-
     public DateTime? StartDate { get; set; }
-
     public DateTime? DueDate { get; set; }
-
-    public int? FkTypeId { get; set; }
-
-    public int? FkPriorityId { get; set; }
-
-    public virtual Priority? FkPriority { get; set; }
-
-    public virtual Type? FkType { get; set; }
+    public int TypeId { get; set; }
+    public Type Type { get; set; } = null!;
+    public int PriorityId { get; set; }
+    public Priority Priority { get; set; } = null!;
 }
